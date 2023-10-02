74526d34d9b9874d8991e62f6355bffd7f651527
bobcat1993 <toni@bobby-chat.com>
2023-10-02 14:36:51 +0100

    Updated the messaging that refers people for professional help

--- features/burnout_survey.py
+++ features/burnout_survey.py
@@ -46,13 +46,13 @@ def get_burnout_infographic(request):
 	title = 'Bobby can help you reduce your risk of burnout'
 	if percent_burnout > 0.8:
 		# User mostly responding with "often"/"always"
-		title = 'I\'m sorry that you are having a difficult time at the moment. Bobby can teach you skills to help you manage workplace stress and improve your wellbeing. However, if how you are feeling about your work is significantly affecting your mood or your function, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
+		title = 'I\'m sorry that you are having a difficult time at the moment. Bobby can teach you skills to help you manage workplace stress and improve your wellbeing. However, if how you are feeling about your work is significantly affecting your mood, your ability to look after yourself or your performance at work, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
 	elif percent_burnout > 0.6:
 		# User mostly responding with "sometimes"/"often"
-		title = 'I\'m sorry that you are having a tough time at the moment. Bobby can teach you skills to help you manage workplace stress and improve your wellbeing. However, if how you are feeling about your work is significantly affecting your mood or your function, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
+		title = 'I\'m sorry that you are having a tough time at the moment. Bobby can teach you skills to help you manage workplace stress and improve your wellbeing.However, if how you are feeling about your work is significantly affecting your mood, your ability to look after yourself or your performance at work, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
 	elif percent_burnout > 0.4:
 		# User mostly responding with "rarely"/"sometimes"
-		title = 'It sounds like sometimes you could do will a little extra help to manage your work stress. The good news is, Bobby can help you with that and hopefully help you improve your wellbeing score over time. However, if how you are feeling about your work is significantly affecting your mood or your function, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
+		title = 'It sounds like sometimes you could do will a little extra help to manage your work stress. The good news is, Bobby can help you with that and hopefully help you improve your wellbeing score over time. However, if how you are feeling about your work is significantly affecting your mood, your ability to look after yourself or your performance at work, please seek help from a professional. You can find some helpful links on our website, bobby-chat.com/help.'
 	elif percent_burnout > 0.2:
 		# User mostly responding with "never" / "rarely"
 		title = 'It sounds like most of the time you are able to cope well at work, that\'s great. Bobby can help you in those isolated moments and can help you practice techniques to boost your confidence and help you set goals at work.'
