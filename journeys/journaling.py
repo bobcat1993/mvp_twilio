9b878d4937be1414bc725a5c15fa910eea70e351
bobcat1993 <toni@bobby-chat.com>
2023-10-07 10:14:16 +0100

    Added functions to get journaling prompt

---
+++ journeys/journaling.py
@@ -0,0 +1,76 @@
+"""A 7-day Journaling Challenge."""
+from datetime import datetime
+from flask import Flask, jsonify
+
+
+def _days_since_start():
+	# Replace 'YYYY-MM-DD' with your desired start date in the format 'YYYY-MM-DD'
+	start_date_str = '2023-10-09'
+
+	# Convert the start date string to a datetime object
+	start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+
+	# Get the current date
+	current_date = datetime.today()
+
+	# Compute the difference between the current date and the start date
+	days_difference = (current_date - start_date).days
+
+	# Don't allow negative days.
+	days_difference = max(0, days_difference)
+
+	return days_difference
+
+_JOURNAL_PROMPTS = [
+	"""Relive a magical moment! Complete this sentence:  “One of the best experiences of my life was…”. Explain the experience and why you are grateful for it.""",
+	"""Write down 5 things that bring you joy - these can be big or small! What is it about these things that means so much to you?""",
+	"""What three things do you do really well, and when are these things most valuable to you?""",
+	"""What does success mean to you?""",
+	"""If you had all the time and resources you needed, what activities or hobbies would you pursue?""",
+	"""You are doing a great job! How does it feel to read this?""",
+	"""Write a short letter of thanks you’ve always wanted to send, but haven’t got round to doing.  This letter could be to someone else, but it could also be to yourself!"""
+]
+
+_FOLLOW_UP_EXAMPLE_QUESTIONS = [
+"""What was unique about this experience?
+Who was with you, and what did it mean to have them there?""",
+"""How might you be able to have you have more of these things in your life?""",
+"""When are these strengths most useful?
+How do you use your strengths to help others?""",
+"""What does society expect success to mean, and how does your version of success fit with this?
+What do you find valuable about society’s version of success, and what don’t you find valuable about it?""",
+"""Is there a way that you could try or learn this thing now?
+  What’s stopping you or holding you back?
+  How can you prioritise spending more time doing fun activities?""",
+"""Do you believe it?
+When was the last time you said this or heard this?""",
+"""What are you thankful for and why?"""
+]
+
+_FOLLOW_UP_QUESTIONS_SYSTEM_PROMPT = """The assistant is helping the user journal. The assistant has given a prompt and will ask follow up question to help the user explore their thoughts. Questions should be short, friendly and thoughtful.
+
+Here are some example follow up questions, only ask one question at a time:
+{example_questions}"""
+
+_PROMPT_URL = "https://storage.googleapis.com/bobby-chat-journaling/day{day_no}.png"
+
+
+def get_journal_prompt(request):
+	"""Give the journaling prompt of the day"""
+
+	# Get the day number/ index.
+	day_index = _days_since_start()
+
+	# Get the prompt and follow up questions based on the day.
+	# Default to day one if the challenge has not started.
+	idx = day_index % len(_JOURNAL_PROMPTS)
+	prompt = _JOURNAL_PROMPTS[idx]
+	follow_up_questions = _FOLLOW_UP_EXAMPLE_QUESTIONS[idx]
+	
+	return jsonify(
+		day=str(day_index + 1),
+		idx=idx,
+		prompt=prompt,
+		follow_up_questions=follow_up_questions,
+		prompt_url=_PROMPT_URL.format(day_no=day_index + 1)
+	)
---
+++ journeys/test_journaling.py
@@ -0,0 +1,72 @@
+"""Tests for the Journaling journey/challenge."""
+import unittest
+from unittest.mock import Mock
+import boundaries
+from parameterized import parameterized
+from flask import Flask, jsonify, request
+from absl import logging
+import os
+import json
+from datetime import datetime, date
+from freezegun import freeze_time
+
+import sys
+sys.path.append('..')
+sys.path.append('.')
+import utils
+from app import BoundariesStageTwoDatum, UserDatum
+
+from journeys import journaling
+
+app = Flask(__name__)
+
+_TODAY_DATE = date.today()
+_USER_NUMBER = 'whatsapp:+44747981767'
+_USER_ID = boundaries.string_hash(_USER_NUMBER)
+
+
+class TestBoundaries(unittest.TestCase):
+
+	def setUp(self):
+		utils.setup_openai()
+
+	# TODO(toni) Parametrize this.
+	def test_days_since_start(self):
+		# freeze_time args: YYYY-MM-DD
+		with freeze_time('2023-10-11'):
+			diff = journaling._days_since_start()
+			logging.info(f'diff={diff}')
+			assert diff == 2
+
+	_EXPECTED_URL = "https://storage.googleapis.com/bobby-chat-journaling/day{day_no}.png"
+
+	@parameterized.expand([
+		('day1', '2023-10-09', '1',  0, _EXPECTED_URL.format(day_no=1)),
+		('day2', '2023-10-10', '2', 1,  _EXPECTED_URL.format(day_no=2)),
+		('day3', '2023-10-11', '3', 2,  _EXPECTED_URL.format(day_no=3)),
+		('day4', '2023-10-12', '4', 3,  _EXPECTED_URL.format(day_no=4)),
+		('day5', '2023-10-13', '5', 4,  _EXPECTED_URL.format(day_no=5)),
+		('day6', '2023-10-14', '6', 5,  _EXPECTED_URL.format(day_no=6)),
+		('day7', '2023-10-15', '7', 6, _EXPECTED_URL.format(day_no=7)),
+		('day8', '2023-10-16', '8', 0, _EXPECTED_URL.format(day_no=8)),
+		('day_minus_1', '2023-10-08', '1', 0,  _EXPECTED_URL.format(day_no=1)),
+		])
+	def test_get_journal_prompt(self, name, current_date, expected_day, expected_idx, expected_url):
+
+		test_request = Mock()
+		test_request.json = {}
+
+		with freeze_time(current_date):
+			with app.app_context():
+				response = journaling.get_journal_prompt(test_request)
+
+			# Asser the day and URL are correct.
+			response = response.json
+			self.assertEqual(response['day'], expected_day)
+			self.assertEqual(response['idx'], expected_idx)
+			self.assertEqual(response['prompt_url'], expected_url)
+
+if __name__ == '__main__':
+	unittest.main()
+
+
