9b6d24bedde4def0b9a344531acdf4af9198926e
bobcat1993 <toni@bobby-chat.com>
2024-04-24 16:44:55 +0100

    Add checks before sending messages. Only create one client

--- send_reminders.py
+++ send_reminders.py
@@ -5,6 +5,8 @@ from urllib.request import urlopen
 import json
 import requests
 from twilio.request_validator import RequestValidator
+from absl import logging
+import time
 
 load_dotenv()
 
@@ -24,26 +26,33 @@ signature = validator.compute_signature(assets_url, params={})
 headers = {'X-Twilio-Signature': signature}
 response = requests.get(assets_url, headers=headers)
 
-white_list = response.json()
+# # white_list = response.json()
+white_list = []
+
 
 # Get the person running the script to confirm they want to run this:
 input(f"This will send a message to {len(white_list)} contacts."
 			" Press enter to continue...")
 
+_BODY="""Hi, did you know that journaling can be a great way to process strong emotions -- good or bad. We've added topics to our journaling feature so you can focus on specific areas such as boundary setting, self-care and even time management. There's always an option to Journal Free Style - if you prefer. Just press "Start Journaling" to get started."""
+
+input(f"This is the message you are about to send:\n {_BODY}"
+			"\nAre you sure this is the message you would like to send?")
+
+client = Client(account_sid, auth_token)
+
 for number in white_list:
 	print(f'Sending message to {number}.')
-
-	client = Client(account_sid, auth_token)
 
 	# TODO(toni) Check who has not already checked in today and only 
 	# message them.
 
+	# Look at ngrok to get the delivery stats.
 	message = client.messages.create(
 		from_=f"whatsapp:{whatsapp_from}",
-		body=(
-			'Is there anything from today that\'s been playing on your'
-			' mind? Chat it over with Bobby.'
-			' Just say "Hi" to start a conversation with Bobby.'),
+		# TODO(toni) Make this a variable to be used with a config.
+		# status_callback='https://<myapp>.app/MessageSatus',
+		body=_BODY,
 		to=number,
 	)
 
